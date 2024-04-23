ea21c06a8d51c40548e7ffb86140531d2b238058
nedoratic <nedoratic@gmail.com>
2024-04-23 22:01:48 +0200

    data.jsx file with cards data added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
 import Card from './components/Card.jsx';
 import './App.css';
+import data from './data.jsx';
 
 function App() {
 	return (
