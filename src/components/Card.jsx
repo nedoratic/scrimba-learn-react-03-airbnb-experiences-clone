220aef8f5302635d6d8ebd92e4f7b83f7ad6b103
nedoratic <nedoratic@gmail.com>
2024-04-23 21:18:02 +0200

    star.svg icon added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,11 @@
+import star from '../assets/star.svg';
+
 const Card = () => {
 	return (
 		<div className='card'>
 			<img className='card__image' src='' alt='Card image.' />
 			<div className='card__stats'>
-				<img className='card__stats__rating-star-icon' src='' alt='Rating star icon.' />
+				<img className='card__stats__rating-star-icon' src={star} alt='Rating star icon.' />
 				<div className='card__stats__rating-score'>5.0</div>
 				<div className='card__stats__rating-number'>(6)</div>
 				<div className='card__stats__separator'>•</div>
