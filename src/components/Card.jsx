ee207717a1de847872f37e4f89aee3d88e98b03e
nedoratic <nedoratic@gmail.com>
2024-04-23 21:48:55 +0200

    images hardcode test

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,10 @@
 import star from '../assets/star.svg';
+import test from '../images/card-img-1.svg';
 
 const Card = () => {
 	return (
 		<div className='card'>
-			<img className='card__image' src='' alt='Card image.' />
+			<img className='card__image' src={test} alt='Card image.' />
 			<div className='card__stats'>
 				<img className='card__stats__rating-star-icon' src={star} alt='Rating star icon.' />
 				<div className='card__stats__rating-score'>5.0</div>
